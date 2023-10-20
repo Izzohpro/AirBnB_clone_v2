2d9d447e8bbbdc99e37068256e83ac74e96ec6c6
Izzohpro <okparafrankline@gmail.com>
2023-10-19 23:12:43 -0700

    Python route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -16,3 +16,22 @@ def desplay_hbnb():
     """Print hbnb"""
     return "HBNB"
 
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_cText(text):
+    """Print C with passed variable"""
+    text = text.replace("_", " ")
+    return "C %s" % (text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_pythonText(text='is cool'):
+    """ Function called with /python/<text> route """
+    if text != 'is cool':
+        text = text.replace('_', ' ')
+    return 'Python %s' % (text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
